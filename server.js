9b0d9961cb8880b2820611caf236f92860ac02f2
jankolubbe2004-droid <jankolubbe2004@gmail.com>
2026-02-03 13:08:03 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+const publicDir = __dirname;
+
+const contentTypes = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml",
+};
+
+const server = http.createServer((req, res) => {
+  const requestPath = req.url === "/" ? "/index.html" : req.url;
+  const safePath = path.normalize(requestPath).replace(/^\.+/, "");
+  const filePath = path.join(publicDir, safePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Not found");
+      return;
+    }
+
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { "Content-Type": contentTypes[ext] || "application/octet-stream" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Life OS server running on http://localhost:${port}`);
+});
